8fa9f9d8ff0860928d01d80b1a6d63fc31740a97
Thiago Ferreira <thiagiferreira9594@gmail.com>
2024-06-30 14:43:56 -0300

    first commit

---
+++ src/main/java/com/tf/literatura/LiteraturaApplication.java
@@ -0,0 +1,21 @@
+package com.tf.literatura;
+
+import com.tf.literatura.main.Main;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaApplication implements CommandLineRunner {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(LiteraturaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Main main = new Main();
+        main.menu();
+    }
+}
---
+++ src/main/java/com/tf/literatura/main/Main.java
@@ -0,0 +1,7 @@
+package com.tf.literatura.main;
+
+public class Main {
+    public void menu(){
+        System.out.println("Hi!");
+    }
+}
